1320ac99276c1d6eaa54ae42740a32675b1da658
yoshikouki <yoshikouki@gmail.com>
2022-05-10 18:22:04 +0900

    URI.encode is obsolete over Ruby 3.0

--- lib/swagger_client/configuration.rb
+++ lib/swagger_client/configuration.rb
@@ -175,7 +175,7 @@ module SwaggerClient
 
     def base_url
       url = "#{scheme}://#{[host, base_path].join('/').gsub(/\/+/, '/')}".sub(/\/+\z/, '')
-      URI.encode(url)
+      URI.encode_www_form(url)
     end
 
     # Gets API key (with prefix if set).
